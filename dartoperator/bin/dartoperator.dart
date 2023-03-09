f0ec238e517f015fe194f40b469dabffc8e93854
Sadman Shahriar <118925483+SadmanShahriar@users.noreply.github.com>
2023-03-10 03:07:07 +0600

    Add files via upload

---
+++ dartoperator/bin/dartoperator.dart
@@ -0,0 +1,13 @@
+import 'package:dartoperator/dartoperator.dart' as dartoperator;
+
+void main() {
+  // int a = 88;
+  // int b = 66;
+  // var result = a/b;
+  // print(result);
+
+  String color = 'red';
+  var result = color== 'red'?'color is red':'unknown';
+
+  print(result);
+}
