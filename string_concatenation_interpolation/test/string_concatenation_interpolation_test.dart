c9d97b1cf3ab526f23f8bd5c91800b632ba6688a
Sadman Shahriar <118925483+SadmanShahriar@users.noreply.github.com>
2023-03-13 20:05:17 +0600

    Delete string_concatenation_interpolation directory

--- string_concatenation_interpolation/test/string_concatenation_interpolation_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:string_concatenation_interpolation/string_concatenation_interpolation.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
